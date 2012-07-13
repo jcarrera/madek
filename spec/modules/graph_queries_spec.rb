4fb57421c16b0d5de590761302c384a9834ebbb6
Thomas Schank <DrTom@schank.ch>
2012-07-13 10:05:20 +0200

    Added a media_resource to the graph_querie spec

--- spec/modules/graph_queries_spec.rb
+++ spec/modules/graph_queries_spec.rb
@@ -3,30 +3,39 @@ require 'spec_helper'
 describe GraphQueries do
 
   before :all do
-    @top_set1 = FactoryGirl.create :media_set
 
-    @child_11 = FactoryGirl.create :media_set
+    #
+    #
+    # 1 -> 11 -> 111
+    #   -> 12 -> 121 
+    #
+    # 1 -> 21
+    #
+
+    @owner = FactoryGirl.create :user
+    @viewer = FactoryGirl.create :user
+
+    @top_set1 = FactoryGirl.create :media_set, user: @owner
+    @child_11 = FactoryGirl.create :media_set, user: @owner
     @top_set1.children << @child_11
 
-    @child_111 = FactoryGirl.create :media_set
+    @child_111 = FactoryGirl.create :media_set, user: @owner
     @top_set1.children << @child_111
 
-
-    @child_12 = FactoryGirl.create :media_set
+    @child_12 = FactoryGirl.create :media_set, user: @owner
     @top_set1.children << @child_12
+    @child_121 = FactoryGirl.create :media_resource, user: @owner
+    @child_12.children << @child_121
 
-
-    @top_set2 = FactoryGirl.create :media_set
-
-    @child_21 = FactoryGirl.create :media_set
+    @top_set2 = FactoryGirl.create :media_set, user: @owner
+    @child_21 = FactoryGirl.create :media_set, user: @owner
     @top_set2.children << @child_21
-
   end
 
   describe "Computing all the descendants of " do
     describe "top_set1" do
-      it "should have 3 children" do
-        GraphQueries.descendants(@top_set1).size.should == 3
+      it "should have 4 children" do
+        GraphQueries.descendants(@top_set1).size.should == 4
       end
     end
   end
