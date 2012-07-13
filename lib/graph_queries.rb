9f50be6cf7ac7b58266ab7a5748e11fff79b3fcb
Thomas Schank <DrTom@schank.ch>
2012-07-13 15:17:54 +0200

    generalized grapy queries

--- lib/graph_queries.rb
+++ lib/graph_queries.rb
@@ -1,8 +1,7 @@
 module GraphQueries
 
-  def self.reachable_arcs media_set
-
-    base_query= <<-SQL   
+  def self.reachable_arcs_query media_set
+    "
       WITH RECURSIVE pair(p,c) as
       (
           SELECT parent_id as p, child_id as c FROM media_resource_arcs 
@@ -14,29 +13,16 @@ module GraphQueries
       SELECT id FROM media_resource_arcs, pair
         WHERE media_resource_arcs.parent_id = pair.p
         AND media_resource_arcs.child_id = pair.c
-    SQL
+    "
+  end
 
-    MediaResourceArc.where(" id in ( #{base_query})")
+  def self.reachable_arcs media_set
+    MediaResourceArc.where(" id in ( #{reachable_arcs_query(media_set)} )")
   end
 
   def self.descendants media_set
-
-    base_query= <<-SQL   
-      WITH RECURSIVE pair(p,c) as
-      (
-          SELECT parent_id as p, child_id as c FROM media_resource_arcs 
-            WHERE parent_id = #{media_set.id}
-        UNION
-          SELECT parent_id as p, child_id as c FROM pair, media_resource_arcs
-            WHERE parent_id = pair.c
-      ) 
-      SELECT c FROM pair
-    SQL
-
-
-    MediaResource.where("media_resources.id in ( #{base_query} )")
+    MediaResource.where(" id in ( #{reachable_arcs(media_set).select("child_id").to_sql} ) ")
   end
-
  
 end
 
