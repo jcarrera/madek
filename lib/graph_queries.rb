867a16392bd2c0e4ee93417a0fd98a58b329a817
Thomas Schank <DrTom@schank.ch>
2012-08-06 16:17:37 +0200

    rewrote graph query to accept array or resource

--- lib/graph_queries.rb
+++ lib/graph_queries.rb
@@ -2,12 +2,12 @@ require 'set'
 
 module GraphQueries
 
-  def self.reachable_arcs_query media_set
+  def self.reachable_arcs_query media_resource_ids
     "
       WITH RECURSIVE pair(p,c) as
       (
           SELECT parent_id as p, child_id as c FROM media_resource_arcs 
-            WHERE parent_id = #{media_set.id}
+            WHERE parent_id in (#{media_resource_ids})
         UNION
           SELECT parent_id as p, child_id as c FROM pair, media_resource_arcs
             WHERE parent_id = pair.c
@@ -18,11 +18,18 @@ module GraphQueries
     "
   end
 
-  def self.reachable_arcs media_set
+  def self.reachable_arcs media_resource_s
+    ids = 
+      if media_resource_s.is_a? Array
+        media_resource_s.map(&:id)
+      else
+        [media_resource_s.id]
+      end
+
     if SQLHelper.adapter_is_postgresql? 
-      MediaResourceArc.where(" id in ( #{reachable_arcs_query(media_set)} )")
+      MediaResourceArc.where(" id in ( #{reachable_arcs_query(ids)} )")
     else 
-      arc_ids = Set.new(MediaResourceArc.where("parent_id = ?",media_set.id).map(&:id))
+      arc_ids = Set.new(MediaResourceArc.where("parent_id in (?)",ids).map(&:id))
       begin
         arcs = MediaResourceArc.where("id in ( ? )",arc_ids)
         prev_arc_ids = arc_ids
@@ -32,9 +39,10 @@ module GraphQueries
     end
   end
 
-  def self.descendants media_set
-    MediaResource.where(" id in ( #{reachable_arcs(media_set).select("child_id").to_sql} ) ")
+  def self.descendants media_set_s
+    MediaResource.where(" id in ( #{reachable_arcs(media_set_s).select("child_id").to_sql} ) ")
   end
- 
+
+
 end
 
