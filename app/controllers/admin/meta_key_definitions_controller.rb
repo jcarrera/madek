c26b00fc95850aa3754f3c873e6113077fac6567
Thomas Schank <DrTom@schank.ch>
2012-08-08 15:21:45 +0200

    Requesting the form

--- app/controllers/admin/meta_key_definitions_controller.rb
+++ app/controllers/admin/meta_key_definitions_controller.rb
@@ -4,4 +4,15 @@ class Admin::MetaKeyDefinitionsController < Admin::AdminController
     @meta_key_definitions = MetaKeyDefinition.all
   end
 
+  def edit
+    @meta_key_definition = MetaKeyDefinition.find(params[:id])
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  def update 
+    binding.pry
+  end
+
 end
